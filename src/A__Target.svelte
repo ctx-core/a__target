fdf74d5210fabe930a92a8783c0dfd7ef473f4f6
Brian Takita <brian.takita@gmail.com>
2021-11-16 21:22:55 -0600

    formatting

--- src/A__Target.svelte
+++ src/A__Target.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-export let href:string
-export let target:string|null|undefined = null
-export let alt = ''
-export let title = ''
-let target__
+export let href:string, target:string|null|undefined = null, alt = '', title = ''
+let target__:string
 $: target__ = target || href.split(':').length > 1 ? '_new' : '_self'
 </script>
 
